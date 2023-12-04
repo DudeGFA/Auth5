85534d4b2619b9f7fb1417bdad270d71547a9e1c
Mahlet Seifu <akotet2123@gmail.com>
2023-12-04 17:02:41 +0300

    fix

--- auth5_Django/Auth5/Account/views.py
+++ auth5_Django/Auth5/Account/views.py
@@ -1,67 +1,25 @@
-from django.http import JsonResponse
-from rest_framework_simplejwt import exceptions
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import generics, permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .serializers import CustomLoginSerializer, LogoutSerializer, RegistrationSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import LogoutSerializer, RegistrationSerializer
 
 
 class RegistrationAPIView(generics.GenericAPIView):
     serializer_class = RegistrationSerializer
     permission_classes = []
 
-    def get(self, request):
-        return render(request, "signup.html")
-
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return render(
-                request,
-                "signup_success.html",
-                {"email": serializer.validated_data["email"]},
-            )
-        else:
-            return render(request, "signup.html", {"errors": serializer.errors})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(TokenObtainPairView):
-    serializer_class = CustomLoginSerializer
-    template_name = "login.html"
-    login_success_template = "login_success.html"
-
-    def get(self, request):
-        return render(request, self.template_name)
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        try:
-            serializer.is_valid(raise_exception=True)
-        except Exception as e:
-            print("Exception caught:", e)
-            # return render(request, "login.html", {"errors": str(e)})
-            return JsonResponse({"error": "Invalid credentials"}, status=400)
-
-        user = serializer.user
-        refresh, access = self.get_tokens_for_user(user)
-
-        return render(
-            request,
-            self.login_success_template,
-            {
-                "refresh_token_from_django_view": str(refresh),
-                "access_token_from_django_view": str(access),
-            },
-        )
-
-    def get_tokens_for_user(self, user):
-        refresh = RefreshToken.for_user(user)
-        access = refresh.access_token
-        return refresh, access
+    def post(self, *args, **kwargs):
+        return super().post(*args, **kwargs)
 
 
 class LoginRefreshView(TokenRefreshView):
