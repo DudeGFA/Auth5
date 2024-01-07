8978e6cd06b92d2e0371e28f289fec632d534099
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2024-01-07 17:23:18 +0100

    update requirements.txt

--- auth5_Django/Auth5/Auth5/settings.py
+++ auth5_Django/Auth5/Auth5/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = literal_eval(os.getenv("ALLOWED_HOSTS", "[]"))
+ALLOWED_HOSTS = ['*']
 
 # AUTH_USER_MODEL = "Account.CustomUser"
 
