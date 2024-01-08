25ff44331c2947143ae1019cba27f339c6b4c866
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2024-01-08 22:59:57 +0100

    update status message when all fields are unauthorized

--- auth5_Django/Auth5/dashboard/views.py
+++ auth5_Django/Auth5/dashboard/views.py
@@ -164,5 +164,5 @@ class FetchDataView(generics.GenericAPIView):
             else:
                 invalid[field] = 'Undefined field'
         if (payload == {}):
-            return Response({'response_content': invalid})
+            return Response({'response_content': invalid}, status=status.HTTP_200_OK)
         return self.send_post_request(payload, invalid)
